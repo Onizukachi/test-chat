9377d59cfa8d8eb45bb8e4edd9a7de4d00746e36
Onizukachi <dark_sao@mail.ru>
2023-12-24 22:44:16 +0300

    dont allow not authorized users see form to create messages

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,9 +5,8 @@ class LikesController < ApplicationController
     room = message.room
     @like = Like.new(user: user, message_id: params[:message_id])
     @like.save
-  
+
     message.broadcast_replace_to room, locals: { message: message.reload }
     message.broadcast_replace_to [user, room], locals: { message: message.reload }
-    
   end
 end
