964fdc41661f009624cd9ab8d53fa65488f0c742
Onizukachi <dark_sao@mail.ru>
2023-12-25 11:06:03 +0300

    docker compose

---
+++ entrypoints/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+echo "bundle install..."
+bundle check || bundle install
+
+bundle exec rake db:create
+bundle exec rake db:migrate
+
+exec "$@"
